4ba7058cfb18b6e2e5abb3ba38efd3398a02f675
Dmitry Shechtman <damnian@gmail.com>
2017-07-11 01:16:53 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Writers.Camera.Props/PropsCameraWriter.cs
@@ -0,0 +1,38 @@
+﻿using Net.Chdk.Meta.Model.Camera;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Meta.Writers.Camera.Props
+{
+    public abstract class PropsCameraWriter<TCamera, TModel, TCard>
+        where TCamera : CameraData<TCamera, TModel, TCard>
+        where TModel : CameraModelData
+        where TCard : CardData
+    {
+        public void WriteCameras(string path, IDictionary<string, TCamera> cameras)
+        {
+            using (var writer = File.CreateText(path))
+            {
+                WriteCameras(writer, cameras);
+            }
+        }
+
+        private void WriteCameras(TextWriter writer, IDictionary<string, TCamera> cameras)
+        {
+            foreach (var kvp in cameras)
+            {
+                WriteModels(writer, kvp.Key, kvp.Value);
+            }
+        }
+
+        private void WriteModels(TextWriter writer, string id, TCamera camera)
+        {
+            foreach (var model in camera.Models)
+            {
+                WriteModel(writer, id, model);
+            }
+        }
+
+        protected abstract void WriteModel(TextWriter writer, string id, TModel model);
+    }
+}
